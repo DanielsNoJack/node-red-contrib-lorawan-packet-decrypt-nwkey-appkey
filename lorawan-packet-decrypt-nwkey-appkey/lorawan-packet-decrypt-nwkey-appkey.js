1db5603de40648aff6cb30091e9fd70672ae7401
DanielsNoJack <45598783+DanielsNoJack@users.noreply.github.com>
2022-12-23 19:04:20 +0100

    Updated lorawan-packet-decrypt-nwkey-appkey.js
    Can send msg.nsk and msg.ask instead of config

--- lorawan-packet-decrypt-nwkey-appkey/lorawan-packet-decrypt-nwkey-appkey.js
+++ lorawan-packet-decrypt-nwkey-appkey/lorawan-packet-decrypt-nwkey-appkey.js
@@ -3,46 +3,75 @@
 // 2021-Mar-21
 module.exports = function(RED) {
 
-    var lora_packet = require('lora-packet');
+    var lora_packet = require('lora-packet')
     
     function lorawandecrypt(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+        RED.nodes.createNode(this, config)
+        var node = this
 
         node.on('input', function(msg) {
             if(msg.payload.length>1){
-
-
                 if(msg.payload !== undefined){
                     var indata = msg.payload
-                    var packet = lora_packet.fromWire(new Buffer(msg.payload, 'base64'));
+                    var packet = lora_packet.fromWire(new Buffer(msg.payload, 'base64'))
                     msg.payload={}
-                    msg.payload.in = indata   
-                    var NwkSKey = new Buffer(config.nsw, 'hex');
+                    msg.payload.in = indata
+                    var nsw = "";
+                    if (config.nsw && config.nsw.length>0)
+                    {   
+                        nsw = config.nsw
+                    }
+                    else if (msg.nsk && msg.nsk.length>0)
+                    {
+                        nsw = msg.nsk
+                    }
+                    else
+                    {
+                        let err = "Error, Missing Network Secret Key!"
+                        done ? done(err) : node.error(err, msg)
+                        node.send(null)
+                        return
+                    }
+                    var NwkSKey = new Buffer(nsw, 'hex')
                     if(lora_packet.verifyMIC(packet, NwkSKey)){
-                        var AppSKey = new Buffer(config.asw, 'hex');
-                        msg.payload.out = lora_packet.decrypt(packet, AppSKey, NwkSKey).toString('hex');
-                        msg.payload.buffers = packet.getBuffers();
-                        node.status({});
-                        node.send(msg);
+                        var asw = "";
+                        if (config.asw && config.asw.length>0)
+                        {   
+                            asw = config.asw
+                        }
+                        else if (msg.ask && msg.ask.length>0)
+                        {
+                            asw = msg.ask
+                        }
+                        else
+                        {
+                            let err = "Error, Missing Application Secret Key!"
+                            done ? done(err) : node.error(err, msg)
+                            node.send(null)
+                            return
+                        }
+                        var AppSKey = new Buffer(asw, 'hex')
+                        msg.payload.out = lora_packet.decrypt(packet, AppSKey, NwkSKey).toString('hex')
+                        msg.payload.buffers = packet.getBuffers()
+                        node.status({})
+                        node.send(msg)
 
                     } else {
-                        let err = "Network Key issue! Raw packet: " + packet;
-                        done ? done(err) : node.error(err, msg);
-                        node.send(null);
+                        let err = "Network Key issue! Raw packet: " + packet
+                        done ? done(err) : node.error(err, msg)
+                        node.send(null)
                     }
                 } else {
-                    node.send(null);
+                    node.send(null)
                 }
             } else {
-                node.send(null);
+                node.send(null)
             }
 
-        });
+        })
 
         node.on("close", function() {
-        });
+        })
     }
-
-    RED.nodes.registerType("lorawan-packet-decrypt-nwkey-appkey", lorawandecrypt);
-};
+    RED.nodes.registerType("lorawan-packet-decrypt-nwkey-appkey", lorawandecrypt)
+}
